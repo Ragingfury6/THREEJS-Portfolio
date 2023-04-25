cd880d87001a70599842ffc10ee4e983f2ab059a
Ragingfury6 <trentsuperman@gmail.com>
2023-04-25 15:22:40 -0700

    Camera Changes (Major)

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@ import './style.css';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
 import Experience from './Experience.js';
+import { Camera } from './Camera';
+import { Suspense } from 'react';
+import { Portfolio } from './Portfolio';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
   <Canvas
-    camera={{
-      fov: 45,
-      near: 0.1,
-      far: 2000,
-      position: [0.6, 3, 4],
-      // position: [0, 10, 10],
-    }}>
-    <Experience />
+  // camera={{
+  //   fov: 45,
+  //   near: 0.1,
+  //   far: 2000,
+  //   // position: [0.6, 3, 4],
+  //   position: [0, 10, 10],
+  // }}
+  >
+    {/* <Suspense fallback={null}>
+      <Camera />
+    </Suspense>
+    <Experience /> */}
+    <Portfolio />
   </Canvas>
 );
